93b0bc67314b82295ceeeddc21e63549ddd69724
PeterYR <ziyuzhang01@gmail.com>
2024-10-31 23:16:24 -0500

    create mess

---
+++ main2.py
@@ -0,0 +1,115 @@
+import pandas as pd
+import requests
+import sys
+
+import pengstats
+
+PETERYR_URL = "https://docs.google.com/spreadsheets/d/1PPtMMKODlSgosKYefDwiIs68elS_t5SrltvBC2s-YD0/export?gid=0&range=A:B&format=csv"
+GAME_DATA_URL = "https://raw.githubusercontent.com/Kengxxiao/ArknightsGameData_YoStar/main/en_US/gamedata/excel/item_table.json"
+
+NAME_OVERRIDES = {
+    "Purchase Certificate": "Shop Voucher",
+    # "Transmuted Salt Agglomerate": "Aggloromated Salt",
+    "Nucleic Crystal Sinter": "烧结核凝晶",
+    "Skill Summary - 1": "Skill Summary 1",
+    "Skill Summary - 2": "Skill Summary 2",
+    "Skill Summary - 3": "Skill Summary 3",
+}
+# manage name discrepancies with interface spreadsheet
+# { actual name : name on sheet }
+
+SANITY_VALUE_OVERRIDES = {}
+
+# add overrides for chips
+for op_class in [
+    "Vanguard",
+    "Guard",
+    "Defender",
+    "Sniper",
+    "Caster",
+    "Medic",
+    "Supporter",
+    "Specialist",
+]:
+    for tier, chip_type in [
+        ("T1", "Chip"),
+        ("T2", "Chip Pack"),
+        ("T3", "Dualchip"),
+    ]:
+        NAME_OVERRIDES[f"{op_class} {chip_type}"] = f"{tier} Chip"
+
+
+def get_id_to_name() -> dict[str, str]:
+    """Load map of itemId (from game data) to item name (for sanval map)"""
+    game_data: dict = requests.get(GAME_DATA_URL).json()["items"]
+    id_to_name: dict[str, str] = {}
+    for id, data in game_data.items():
+        name = data["name"]
+
+        # use spreadsheet override if found
+        id_to_name[data["itemId"]] = NAME_OVERRIDES.get(name, name)
+    return id_to_name
+
+
+def get_sanval_map() -> dict[str, float]:
+    """Load sanity values map from interface Google Sheet"""
+    peteryr_df = pd.read_csv(PETERYR_URL, header=None, names=["en_name", "sanval"])
+    peteryr_sanvals = {}
+    for _, row in peteryr_df.iterrows():
+        en_name = row["en_name"]
+        sanval = row["sanval"]
+        peteryr_sanvals[en_name] = sanval
+    return peteryr_sanvals
+
+
+def main():
+    # if len(sys.argv) < 3:
+    #     print(f"Usage: {sys.argv[0]} <stage_id> <sanity cost> [extra LMD/san]")
+    #     sys.exit()
+
+    # sanity_cost = int(sys.argv[2])
+    extra_lmd = 20
+
+    # if len(sys.argv) >= 4:
+    #     extra_lmd = int(sys.argv[3])
+    # assert sanity_cost > 0
+
+    stage_data = requests.get(
+        "https://penguin-stats.io/PenguinStats/api/v2/stages"
+    ).json()
+    sancosts = {}
+    for stage in stage_data:
+        sancosts[stage["stageId"]] = stage["apCost"]
+
+    # print(sancosts)
+
+    matrix = pengstats.get_matrix()
+
+    id_to_name = get_id_to_name()
+    peteryr_sanvals = get_sanval_map()
+
+    stages = [
+        s
+        for s in matrix["stageId"].unique()
+        if s.startswith("main_14") or s.startswith("tough_14")
+    ]
+    results = {}
+    for stage in sorted(stages):
+        sanity_cost = sancosts[stage]
+        rates = pengstats.stage_rates(stage, matrix)
+        # print("ID", "Name", "Value", "Rate", sep=", ")
+        total = 0
+        for id, rate in rates.items():
+            name = id_to_name.get(id, "n/a")
+            sanval = peteryr_sanvals.get(name, 0)
+            # print(f"{id}, {name}, {sanval:.4f}, {rate:.4f}")
+            total += sanval * rate
+
+        total += (extra_lmd + 12) * sanity_cost * peteryr_sanvals["LMD"]
+
+        # print(f"\n{stage} efficiency:", total / sanity_cost)
+        print(f"{stage},{total / sanity_cost:.4f}")
+
+
+if __name__ == "__main__":
+    main()
--- pengstats.py
+++ pengstats.py
@@ -2,6 +2,10 @@ import pandas as pd
 import requests
 
 
+END_TIMESTAMP = 1715716800000
+ELEVATED_RATES = True
+
+
 # Penguin stats docs: https://penguin-stats.cn/swagger/index.html
 
 PENGUIN_URL = "https://penguin-stats.io/PenguinStats/api/v2"
@@ -16,11 +20,21 @@ PENGUIN_COLUMNS = {
 def get_matrix():
     """Request and load drop matrix into memory"""
 
-    global matrix
-
     r = requests.get(PENGUIN_URL + "/result/matrix", params={"show_closed_zones": True})
+    data = r.json()["matrix"]
+
+    filtered = []
+    for entry in data:
+        stage_id = entry["stageId"]
+        if stage_id.startswith("main_14") or stage_id.startswith("tough_14"):
+            if ELEVATED_RATES and entry["end"] != END_TIMESTAMP:
+                continue  # only keep event rates
+            if not ELEVATED_RATES and entry["end"] == END_TIMESTAMP:
+                continue  # remove event rates
+        filtered.append(entry)
+
     matrix = pd.DataFrame.from_records(
-        r.json()["matrix"],
+        filtered,
         columns=PENGUIN_COLUMNS.keys(),
     )
 
